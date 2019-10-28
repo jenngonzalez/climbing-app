79e860868ed4e25cc4296c26000f5293ef8f7ffd
Jenn Gonzalez <jgonz9873@gmail.com>
2019-10-27 18:16:50 -0700

    WIP weather widget using geolocation

--- src/routes/ClimbingPlan/ClimbingPlan.js
+++ src/routes/ClimbingPlan/ClimbingPlan.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './ClimbingPlan.css';
 
 export default class ClimbingPlan extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            lat: null,
+            lng: null,
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.setState({ lat: position.coords.latitude, lng: position.coords.longitude });
+        }, err => console.log(err)
+        );
+    }
+
+
     render() {
         return (
-            <div className='climbing-plan'>
-                <section className='weather'>WEATHER</section>
+            <div className='climbing-plan' ref={el => (this.div = el)}>
+                <section className='weather'>
+                    WEATHER
+                    <p>Latitude: {this.state.lat}</p>
+                    <p>Longitude: {this.state.lng}</p>
+                </section>
                 <section className='map'>MAP</section>
                 <section className='list'>LIST OF ROUTES</section>
             </div>
