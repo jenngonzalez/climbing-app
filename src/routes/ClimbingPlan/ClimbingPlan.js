bc3755e28d5ec38486bcb6d81709d07938e71b19
Jenn Gonzalez <jgonz9873@gmail.com>
2019-10-29 20:33:29 -0700

    display data and icons from weather API

--- src/routes/ClimbingPlan/ClimbingPlan.js
+++ src/routes/ClimbingPlan/ClimbingPlan.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import Skycons from 'react-skycons'
 import GetWeatherApiService from '../../services/getWeather-api-service';
 import './ClimbingPlan.css';
+
+
 
 export default class ClimbingPlan extends Component {
 
@@ -10,7 +14,12 @@ export default class ClimbingPlan extends Component {
             lat: null,
             lng: null,
             error: null,
-            currentTemp: null
+            currentTemp: null,
+            currentHumidity: null,
+            currentSummary: null,
+            currentTime: null,
+            timeZone: null,
+            weatherIcon: null
         }
     }
 
@@ -26,12 +35,23 @@ export default class ClimbingPlan extends Component {
             .then((position) => {
                 const lat = position.coords.latitude
                 const lng = position.coords.longitude
-                console.log('lat', lat, 'lng', lng)
                 GetWeatherApiService.getWeather(lat, lng)
                     .then(weatherData=> {
                         console.log(weatherData)
                         const currentTemp = weatherData.currently.temperature
-                        this.setState({ currentTemp: currentTemp })
+                        const currentSummary = weatherData.currently.summary
+                        const currentHumidity = weatherData.currently.humidity
+                        const currentTime = weatherData.currently.time
+                        const timeZone = weatherData.currently.timezone
+                        const weatherIcon = weatherData.currently.icon.toUpperCase()
+                        this.setState({
+                            currentTemp: currentTemp,
+                            currentHumidity: currentHumidity,
+                            currentSummary: currentSummary,
+                            currentTime: currentTime,
+                            timeZone: timeZone,
+                            weatherIcon: weatherIcon
+                        })
                     })
             })
             .catch((err) => {
@@ -41,13 +61,26 @@ export default class ClimbingPlan extends Component {
 
 
     render() {
+        const unixTimestamp = this.state.currentTime
+        const tz = this.state.timeZone
+        const icon = this.state.weatherIcon
+
         return (
             <div className='climbing-plan' ref={el => (this.div = el)}>
                 <section className='weather'>
                     WEATHER
-                    <p>Latitude: {this.state.lat}</p>
-                    <p>Longitude: {this.state.lng}</p>
-                    <p>Current Temp: {this.state.currentTemp}</p>
+                    <p>Current Time: <Moment unix tz={tz} format="MMM Do YYYY hh:mm a">{unixTimestamp}</Moment></p>
+                    <p>Current Temp: {this.state.currentTemp} &deg;</p>
+                    <p>Current Humidity: {this.state.humidity}</p>
+                    <p>Summary: {this.state.currentSummary}</p>
+                    <div className='skycon'>
+                        <Skycons 
+                            color='white' 
+                            icon={icon}
+                            autoplay={true}
+                            id='weather-icon'
+                        />
+                    </div>
                 </section>
                 <section className='map'>MAP</section>
                 <section className='list'>LIST OF ROUTES</section>
