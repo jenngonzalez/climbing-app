0c2956c7bb263a6575415ad3a92bea2d3ef53d3d
Jenn <45503445+jenngonzalez@users.noreply.github.com>
2019-11-11 11:10:48 -0800

    005 add climbs (#7)
    * add climbs form, POST to server
    
    * success handling

--- src/routes/ClimbingTrack/ClimbingTrack.js
+++ src/routes/ClimbingTrack/ClimbingTrack.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AddClimbsForm from '../../components/AddClimbs/AddClimbs';
 import './ClimbingTrack.css';
 
 export default class ClimbingTrack extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            showAddForm: false
+        }
+    }
+
+    showAddForm = () => {
+        this.setState({
+            showAddForm: true
+        })
+    }
+
+    handleCancel = () => {
+        this.setState({
+            showAddForm: false
+        })
+    }
+
+    handleSubmitSuccess = () => {
+        this.setState({
+            showAddForm: false
+        })
+        alert('Climb successfully added')
+        // TODO:
+        // communicate success to user
+        // update the "climbs" section to show the new climb (sort by date?)
+    }
+
     render() {
         return (
             <div className='climbing-track'>
-                <section className='climbs'>
-                    CLIMBS
-                </section>
-                <section className='add'>
-                    +
-                </section>
+                <div className='add-form-container'>
+                    <button onClick={this.showAddForm}>
+                        Click to Add a Climb!
+                    </button>
+                    {this.state.showAddForm && <AddClimbsForm onAddSuccess={this.handleSubmitSuccess} onCancel={this.handleCancel}  />}
+                </div>
+                <div className='user-climbs-container'>
+                    SHOW (get) CLIMBS
+                </div>
             </div>
         )
     }
