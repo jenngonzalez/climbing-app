3be2b3feb2069795b9000ed8cf48e2959d187e61
Jenn Gonzalez <jgonz9873@gmail.com>
2019-11-14 21:00:26 -0800

    styling for landing page

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -5,18 +5,21 @@ export default class LandingPage extends Component {
     render() {
         return (
             <div className="landing-page">
-                <section>
+                <div className='info-section'>
+                    Take Your Climbs Outside
+                </div>
+                <div className='landing-info'>
                     <h3>Plan Your Next Climbing Trip</h3>
                     <p>See nearby climbing areas and routes, or search for a specific location. Filter by type and grade of climbs. Check current and future weather. </p>
-                </section>
-                <section>
+                </div>
+                <div className='landing-info'>
                     <h3>Share With Friends</h3>
                     <p>After planning out your trip, save it and get a shareable link to invite friends along.</p>
-                </section>
-                <section>
+                </div>
+                <div className='landing-info'>
                     <h3>Track Your Progress</h3>
                     <p>Keep track of your achievements - save your completed climbs and see statistics for the day, week, month or year.</p>
-                </section>
+                </div>
             </div>
         )
     }
